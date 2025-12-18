b8cd7b87edc23f624cc6f6503050b9e97f19a9f4
Nur Rahmawati <nurrahmawati6603@gmail.com>
2025-12-18 17:59:42 +0700

    Fix workflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 import mlflow
 import mlflow.sklearn
@@ -7,6 +6,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
+# JANGAN set_tracking_uri (biarin default)
+# JANGAN os.environ.pop("MLFLOW_RUN_ID")
+
+# Load data - Pastiin path ini sesuai posisi file pas di-push
 df = pd.read_csv("houseprices_preprocessing/house_data_processed.csv")
 X = df.drop("House_Price", axis=1)
 y = df["House_Price"]
@@ -16,14 +19,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 if not mlflow.active_run():
     mlflow.start_run()
 
-with mlflow.active_run() as run:
-    mlflow.log_param("model_type", "LinearRegression")
-    model = LinearRegression()
-    model.fit(X_train, y_train)
+active_run = mlflow.active_run()
+print(f"Running with ID: {active_run.info.run_id}")
 
-    signature = infer_signature(X_train, model.predict(X_train))
-    mlflow.sklearn.log_model(model, "model", signature=signature)
+model = LinearRegression()
+model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
-    mlflow.log_metric("mse", mean_squared_error(y_test, y_pred))
-    mlflow.log_metric("r2", r2_score(y_test, y_pred))
+# Signature + Metrics
+signature = infer_signature(X_train, model.predict(X_train))
+mlflow.sklearn.log_model(model, "model", signature=signature)
+
+y_pred = model.predict(X_test)
+mlflow.log_metric("mse", mean_squared_error(y_test, y_pred))
+mlflow.log_metric("r2", r2_score(y_test, y_pred))
