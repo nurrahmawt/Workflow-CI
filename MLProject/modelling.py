4bf5ba702ba693ef8765ba0b9e1be129a23f17f2
Nur Rahmawati <nurrahmawati6603@gmail.com>
2025-12-18 15:20:19 +0700

    Add MLflow CI workflow

---
+++ MLProject/modelling.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load dataset
+df = pd.read_csv("houseprices_preprocessing/house_data_processed.csv")
+
+X = df.drop("House_Price", axis=1)
+y = df["House_Price"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+# Manual MLflow logging
+mlflow.log_param("model_type", "LinearRegression")
+mlflow.log_metric("mse", mse)
+mlflow.log_metric("r2", r2)
+
+mlflow.sklearn.log_model(model, artifact_path="model")
+
+print("MSE:", mse)
+print("R2:", r2)
