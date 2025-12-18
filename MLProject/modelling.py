a5f8fd57e7318610fc5f96e517f1cc4f8e6c0505
Nur Rahmawati <nurrahmawati6603@gmail.com>
2025-12-18 17:39:09 +0700

    Fix workflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,5 +1,5 @@
 import os
-os.environ.pop("MLFLOW_RUN_ID", None)
+os.environ.pop("MLFLOW_RUN_ID", None)  # bersihkan run ID lama
 
 import pandas as pd
 import mlflow
@@ -9,28 +9,37 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-mlflow.set_tracking_uri("file:///tmp/mlruns")
+# set tracking URI lokal
+mlflow.set_tracking_uri("file:///tmp/mlruns") 
 
+# load data
 df = pd.read_csv("houseprices_preprocessing/house_data_processed.csv")
 X = df.drop("House_Price", axis=1)
 y = df["House_Price"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-with mlflow.start_run():
+with mlflow.start_run(): 
     mlflow.log_param("model_type", "LinearRegression")
+
     model = LinearRegression()
     model.fit(X_train, y_train)
 
-    signature = infer_signature(X_train, model.predict(X_train))
+    # signature + input example
     input_example = X_train.head(5)
+    signature = infer_signature(X_train, model.predict(X_train))
 
-    mlflow.sklearn.log_model(model, artifact_path="model", signature=signature, input_example=input_example)
+    mlflow.sklearn.log_model(
+        model,
+        artifact_path="model",
+        signature=signature,
+        input_example=input_example
+    )
 
-    mse = mean_squared_error(y_test, model.predict(X_test))
-    r2 = r2_score(y_test, model.predict(X_test))
-    mlflow.log_metric("mse", mse)
-    mlflow.log_metric("r2", r2)
+    # log metrics
+    y_pred = model.predict(X_test)
+    mlflow.log_metric("mse", mean_squared_error(y_test, y_pred))
+    mlflow.log_metric("r2", r2_score(y_test, y_pred))
 
-    print("MSE:", mse)
-    print("R2:", r2)
+    print("MSE:", mean_squared_error(y_test, y_pred))
+    print("R2:", r2_score(y_test, y_pred))
