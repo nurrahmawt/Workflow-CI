d920a5816fbe343e9a62d2de6f64b52e4fcc35b2
Nur Rahmawati <nurrahmawati6603@gmail.com>
2025-12-18 15:34:06 +0700

    Fix MLflow run conflict for CI

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-# Load dataset
+# load data
 df = pd.read_csv("houseprices_preprocessing/house_data_processed.csv")
 
 X = df.drop("House_Price", axis=1)
@@ -16,7 +16,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Train model
 model = LinearRegression()
 model.fit(X_train, y_train)
 
@@ -25,11 +24,10 @@ y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
-# Manual MLflow logging
+# ✅ LOG LANGSUNG (tanpa start_run)
 mlflow.log_param("model_type", "LinearRegression")
 mlflow.log_metric("mse", mse)
 mlflow.log_metric("r2", r2)
-
 mlflow.sklearn.log_model(model, artifact_path="model")
 
 print("MSE:", mse)
